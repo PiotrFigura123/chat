61e0bd3a4e694b56724c9ae1086709d578b34ba6
P.Figura@il-pib.pl <p.figura@il-pib.pl>
2024-01-11 12:07:19 +0100

    users colors

--- src/main/resources/static/js.js
+++ src/main/resources/static/js.js
@@ -1,6 +1,8 @@
 var client = null;
-function showMessage(value, user) {
+var color;
+function showMessage(value, user, userColor) {
   var newResponse = document.createElement('p');
+  newResponse.style.color=userColor;
   newResponse.appendChild(document.createTextNode(user));
   newResponse.appendChild(document.createTextNode(": "));
   newResponse.appendChild(document.createTextNode(value));
@@ -9,14 +11,24 @@ function showMessage(value, user) {
 }
 function connect(){
   client = Stomp.client('ws://localhost:8080/chat');
+  color = getRandomColor();
   client.connect({}, function(frame) {
     client.subscribe("/topic/messages", function(message) {
-      showMessage(JSON.parse(message.body).value, JSON.parse(message.body).user)
+      showMessage(JSON.parse(message.body).value, JSON.parse(message.body).user, JSON.parse(message.body).userColor)
     });
   })
 }
 function sendMessage(){
   var messageToSend = document.getElementById('messageToSend').value;
   var user = document.getElementById('user').value;
-  client.send("/app/chat", {}, JSON.stringify({'value': messageToSend, 'user': user}));
+  client.send("/app/chat", {}, JSON.stringify({'value': messageToSend, 'user': user, 'userColor': color}));
+  document.getElementById('messageToSend').value="";
+}
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
