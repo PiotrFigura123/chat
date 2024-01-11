07409c4631440efe0aa0e7969023f013050bfc34
P.Figura@il-pib.pl <p.figura@il-pib.pl>
2024-01-11 11:34:40 +0100

    added user to chat

--- src/main/java/pl/piotrfigura/chat/ChatMessage.java
+++ src/main/java/pl/piotrfigura/chat/ChatMessage.java
@@ -3,6 +3,15 @@ package pl.piotrfigura.chat;
 public class ChatMessage {
 
     private String value;
+    private String user;
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
 
     public ChatMessage(String value) {
         this.value = value;
--- src/main/resources/static/js.js
+++ src/main/resources/static/js.js
@@ -1,6 +1,8 @@
 var client = null;
-function showMessage(value) {
+function showMessage(value, user) {
   var newResponse = document.createElement('p');
+  newResponse.appendChild(document.createTextNode(user));
+  newResponse.appendChild(document.createTextNode(": "));
   newResponse.appendChild(document.createTextNode(value));
   var response = document.getElementById('response');
   response.appendChild(newResponse);
@@ -9,11 +11,12 @@ function connect(){
   client = Stomp.client('ws://localhost:8080/chat');
   client.connect({}, function(frame) {
     client.subscribe("/topic/messages", function(message) {
-      showMessage(JSON.parse(message.body).value)
+      showMessage(JSON.parse(message.body).value, JSON.parse(message.body).user)
     });
   })
 }
 function sendMessage(){
   var messageToSend = document.getElementById('messageToSend').value;
-  client.send("/app/chat", {}, JSON.stringify({'value': messageToSend}));
+  var user = document.getElementById('user').value;
+  client.send("/app/chat", {}, JSON.stringify({'value': messageToSend, 'user': user}));
 }
